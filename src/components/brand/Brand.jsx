74ca882e37acf1085dedd30ed98a6dd9ffebc9c6
Sofía Fornaroli <Sofia0101>
2023-12-19 14:56:17 -0300

    agrego estilos y modifico componentes de Brand y WhatGPT3

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import "./brand.css";
+import {google, slack, shopify, dropbox, atlassian} from "./imports.js";
+
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img className='' src={google} alt="google" />
+      </div>
+      <div>
+        <img className='' src={slack} alt="slack" />
+      </div>
+      <div>
+        <img className='' src={shopify} alt="shopify" />
+      </div>
+      <div>
+        <img className='' src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img className='' src={atlassian} alt="atlassian" />
+      </div>
+    </div>
   )
 }
 
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
 import "./whatgpt3.css";
+import Features from "../features/Features";
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
-  )
-}
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Features />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Features />
+        <Features />
+        <Features />
+      </div>
+    </div>
+  );
+};
 
-export default WhatGPT3
+export default WhatGPT3;
